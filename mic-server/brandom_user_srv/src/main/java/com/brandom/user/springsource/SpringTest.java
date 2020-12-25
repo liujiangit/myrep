37c093dd1b0a59a5a2ae0fd530a30690a18a5e00
liujian <liujian2598@163.com>
2020-12-25 21:21:13 +0800

    提交修改

--- mic-server/brandom_user_srv/src/main/java/com/brandom/user/springsource/SpringTest.java
+++ mic-server/brandom_user_srv/src/main/java/com/brandom/user/springsource/SpringTest.java
@@ -1,5 +1,7 @@
 package com.brandom.user.springsource;
 
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.util.Assert;
 
 /**
@@ -14,9 +16,9 @@ import org.springframework.util.Assert;
 public class SpringTest {
 
     public static void main(String[] args) {
-        /*XmlBeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("springApplication.xml"));
+        XmlBeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("springApplication.xml"));
         Student student = (Student) beanFactory.getBean("student");
-        System.out.println(student.getName());*/
+        System.out.println(student.getName());
         Assert.notNull(null, "EncodedResource must not be null");
 
     }
