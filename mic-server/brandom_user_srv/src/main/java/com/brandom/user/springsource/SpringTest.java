22fde50d7d1a771813794b94c884586d9cd5083c
liujian <liujian2598@163.com>
2020-12-25 18:26:21 +0800

    spring获取bean

---
+++ mic-server/brandom_user_srv/src/main/java/com/brandom/user/springsource/SpringTest.java
@@ -0,0 +1,23 @@
+package com.brandom.user.springsource;
+
+import org.springframework.util.Assert;
+
+/**
+ * 描述:
+ *
+ * @version 0.1
+ * @Author liujian
+ * @date 2020/12/25 18:08
+ * @since JDK1.8
+ */
+
+public class SpringTest {
+
+    public static void main(String[] args) {
+        /*XmlBeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("springApplication.xml"));
+        Student student = (Student) beanFactory.getBean("student");
+        System.out.println(student.getName());*/
+        Assert.notNull(null, "EncodedResource must not be null");
+
+    }
+}
---
+++ mic-server/brandom_user_srv/src/main/java/com/brandom/user/springsource/Student.java
@@ -0,0 +1,23 @@
+package com.brandom.user.springsource;
+
+/**
+ * 描述:
+ *
+ * @version 0.1
+ * @Author liujian
+ * @date 2020/12/25 18:09
+ * @since JDK1.8
+ */
+
+public class Student {
+
+    private String name = "testApr";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
