6085d7a9d4ca706a7c708ca544fe75f9404066df
liujian <liujian2598@163.com>
2021-03-14 18:57:14 +0800

    新建项目

---
+++ mic-server/brandom_repository_srv/src/main/java/com/brandom/repository/BrandomRepositorySrvApplication.java
@@ -0,0 +1,13 @@
+package com.brandom.repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandomRepositorySrvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrandomRepositorySrvApplication.class, args);
+    }
+
+}
---
+++ mic-server/brandom_repository_srv/src/test/java/com/brandom/repository/BrandomRepositorySrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brandom.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrandomRepositorySrvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
