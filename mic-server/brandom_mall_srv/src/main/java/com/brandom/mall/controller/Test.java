8caf957bdd66f62bd36591798169fcc0450bc2e1
liujian302 <liujian2598@163.com>
2021-05-24 20:08:56 +0800

    要撤销的修改

---
+++ mic-server/brandom_mall_srv/src/main/java/com/brandom/mall/controller/Test.java
@@ -0,0 +1,9 @@
+package com.brandom.mall.controller;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("ddd");
+        System.out.println("aaa");
+        System.out.println("dds");
+    }
+}
